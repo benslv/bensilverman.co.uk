49d8d92ce6ea1f0d4bd18078dbf4bd9c88cee417
Ben Silverman <bensilverman10@gmail.com>
2024-11-15 07:24:37 +0000

    notes list

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,48 +1,57 @@
 import { defineCollection, z } from "astro:content";
 
 const postCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    datePublished: z.date().nullable(),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		datePublished: z.date().nullable(),
+	}),
+});
+
+const noteCollection = defineCollection({
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		datePublished: z.date().nullable(),
+	}),
 });
 
 const projectCollection = defineCollection({
-  type: "data",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    url: z.string().url().optional(),
-    repoUrl: z.string(),
-    featured: z.boolean().optional(),
-    emoji: z.string().emoji(),
-  }),
+	type: "data",
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		url: z.string().url().optional(),
+		repoUrl: z.string(),
+		featured: z.boolean().optional(),
+		emoji: z.string().emoji(),
+	}),
 });
 
 const gameCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    dateCompleted: z.date(),
-    rating: z.number().int().min(1).max(5),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		dateCompleted: z.date(),
+		rating: z.number().int().min(1).max(5),
+	}),
 });
 
 const bookCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    id: z.string(),
-    author: z.string(),
-    rating: z.number(),
-    date: z.date(),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		id: z.string(),
+		author: z.string(),
+		rating: z.number(),
+		date: z.date(),
+	}),
 });
 
 export const collections = {
-  posts: postCollection,
-  projects: projectCollection,
-  games: gameCollection,
-  books: bookCollection,
+	posts: postCollection,
+	notes: noteCollection,
+	projects: projectCollection,
+	games: gameCollection,
+	books: bookCollection,
 };
