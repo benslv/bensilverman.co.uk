5f2cc0de89be7a5d0d447ca99c914c11c341c6ce
Ben Silverman <bensilverman10@gmail.com>
2025-12-30 22:49:54 +0000

    Add netlify adapter

--- astro.config.ts
+++ astro.config.ts
@@ -5,8 +5,11 @@ import markdoc from "@astrojs/markdoc";
 import react from "@astrojs/react";
 import keystatic from "@keystatic/astro";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), keystatic()],
-	site: "https://bensilverman.co.uk",
+  integrations: [react(), markdoc(), keystatic()],
+  site: "https://bensilverman.co.uk",
+  adapter: netlify(),
 });
